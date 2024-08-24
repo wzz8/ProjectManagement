186279fe7e39670c286a5abead535fddf6304182
wzz <wzz80@163.com>
2024-08-24 17:18:53 +0800

    add hosting

--- ProjectManagement/App.xaml.cs
+++ ProjectManagement/App.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProjectManagement.Services;
+using ProjectManagement.Views;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace ProjectManagement
 {
@@ -9,6 +15,53 @@ namespace ProjectManagement
     /// </summary>
     public partial class App : Application
     {
+        // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
+        // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+        // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+        // https://docs.microsoft.com/dotnet/core/extensions/configuration
+        // https://docs.microsoft.com/dotnet/core/extensions/logging
+        private static readonly IHost _host = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(c =>
+            {
+                _ = c.SetBasePath(AppContext.BaseDirectory);
+            })
+            .ConfigureServices(
+                (_1, services) =>
+                {
+                    // App Host
+                    _ = services.AddHostedService<ApplicationHostService>();
+
+                    // Main window container with navigation
+                    _ = services.AddSingleton<MainWindow>();
+                    _ = services.AddSingleton<LoginWindow>();
+                    //_ = services.AddSingleton<MainWindowViewModel>();
+                    //_ = services.AddSingleton<INavigationService, NavigationService>();
+                    //_ = services.AddSingleton<ISnackbarService, SnackbarService>();
+                    //_ = services.AddSingleton<IContentDialogService, ContentDialogService>();
+                    //_ = services.AddSingleton<WindowsProviderService>();
+
+                    //// Top-level pages
+                    //_ = services.AddSingleton<DashboardPage>();
+                    //_ = services.AddSingleton<DashboardViewModel>();
+                    //_ = services.AddSingleton<AllControlsPage>();
+                    //_ = services.AddSingleton<AllControlsViewModel>();
+                    //_ = services.AddSingleton<SettingsPage>();
+                    //_ = services.AddSingleton<SettingsViewModel>();
+
+                    //// All other pages and view models
+                    //_ = services.AddTransientFromNamespace("Wpf.Ui.Gallery.Views", GalleryAssembly.Asssembly);
+                    //_ = services.AddTransientFromNamespace(
+                    //    "Wpf.Ui.Gallery.ViewModels",
+                    //    GalleryAssembly.Asssembly
+                    //);
+                }
+            )
+            .Build();
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            _host.Start();
+        }
     }
 
 }
---
+++ ProjectManagement/Services/ApplicationHostService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProjectManagement.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjectManagement.Services
+{
+    internal class ApplicationHostService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ApplicationHostService(IServiceProvider serviceProvider)
+        {
+            // If you want, you can do something with these services at the beginning of loading the application.
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                return Task.CompletedTask;
+            }
+
+            var loginWindow = _serviceProvider.GetRequiredService<LoginWindow>();
+            loginWindow?.Show();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
